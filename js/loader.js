12f526db041f37b37ab0562881dfe40a8f7d0963
sagar-sehgal <sagar.r16@iiits.in>
2020-04-19 00:22:01 +0530

    added coffee mode

--- js/loader.js
+++ js/loader.js
@@ -10,3 +10,18 @@ function toggleModal(){
 	var instance = M.Modal.getInstance($("#modal3"));
 	instance.open();
 }
+
+function coffeeMode(){
+	body=$("body");
+	btn=$(".togglebtn");
+	if(body.hasClass("coffee")===true){
+		body.removeClass("coffee");
+		body.addClass("casual");
+		btn.text("Coffee Mode");
+	}
+	else if(body.hasClass("casual")===true){
+		body.removeClass("casual");
+		body.addClass("coffee");
+		btn.text("Normal Mode");
+	}
+}
